8b1cd809851ff3249cb702bc010c9d6903a3fed5
Alberto Gil <alberto.gil.contractor@bbva.com>
2015-08-13 14:02:01 +0200

    [feat] new gulp tasks and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,41 @@ var sass = require('gulp-sass'),
     rename = require('gulp-rename'),
     gutil = require('gulp-util'),
     minifyCss = require('gulp-minify-css'),
-    sh = require('shelljs'),
     livereload = require('gulp-livereload'),
-    server = require('gulp-server-livereload'),
-    eslint = require('gulp-eslint');
+    connect = require('gulp-connect'),
+    eslint = require('gulp-eslint'),
+    htmlreplace = require('gulp-html-replace'),
+    environments = require('gulp-environments'),
+    del = require('del'),
+    friendlyFormatter = require('eslint-friendly-formatter');
+
+//Set environments
+var development = environments.development;
+var production = environments.production;
 
 // Webserver
 gulp.task('webserver', function() {
-    gulp.src('app')
-        .pipe(server({
-            livereload: true,
-            directoryListing: true,
-            open: true,
-            port: 9500
-        }));
+    connect.server({
+        root: 'dist',
+        livereload: true
+    });
 });
 
-// Lint Task
-gulp.task('lint', function () {
-    return gulp.src(['js/**/*.js'])
-        // eslint() attaches the lint output to the eslint property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failOnError last.
-        .pipe(eslint.failOnError());
+gulp.task('replace', ['cleanDist'], function() {
+    gulp.src('www/index.html')
+        .pipe(development(htmlreplace({
+            // 'css': 'styles.min.css',
+            'core': 'js/core/core.js',
+            'js': 'js/feature/feature.js',
+            'vendors': 'js/vendors/vendors.js'
+        }))
+        .pipe(production(htmlreplace({
+            // 'css': 'styles.min.css',
+            'core': 'js/core/core.min.js',
+            'js': 'js/feature/feature.min.js',
+            'vendors': 'js/vendors/vendors.min.js'
+        }))))
+        .pipe(gulp.dest('dist/'));
 });
 
 // Compile Our Sass
@@ -46,23 +53,59 @@ gulp.task('sass', function() {
         .pipe(livereload());
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'))
+// Concatenate coreJs
+gulp.task('coreJs', ['cleanDist'], function() {
+    return gulp.src('www/app/core/*.js')
+        .pipe(concat('core.js'))
+        .pipe(production(rename('core.min.js')))
+        .pipe(production(uglify()))
+        .pipe(gulp.dest('dist/js/core'))
         .pipe(livereload());
+});
+
+// Concatenate featureJs
+gulp.task('featureJs', ['cleanDist'], function() {
+    return gulp.src(['www/app/**/*.js', '!www/app/core/*.js'])
+        .pipe(concat('feature.js'))
+        .pipe(production(rename('feature.min.js')))
+        .pipe(production(uglify()))
+        .pipe(gulp.dest('dist/js/feature'))
+        .pipe(livereload());
+});
+
+// Concatenate vendors
+gulp.task('vendors', ['cleanDist'], function() {
+    return gulp.src(['node_modules/angular/angular.min.js', 'node_modules/angular-ui-router/build/angular-ui-router.min.js'])
+        .pipe(concat('vendors.js'))
+        .pipe(production(rename('vendors.min.js')))
+        .pipe(production(uglify()))
+        .pipe(gulp.dest('dist/js/vendors'))
+        .pipe(livereload());
+});
+
+// Clean dist folder
+gulp.task('cleanDist', function(cb) {
+    del(['dist'], {force: true}, cb);
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('www/app/**/*.js', ['lint']);
     gulp.watch('scss/*.scss', ['sass']);
 });
 
+// Lint Task
+gulp.task('lint', function () {
+    return gulp.src(['www/app/**/*.js'])
+        .pipe(eslint('.eslintrc'))
+        .pipe(eslint.format(friendlyFormatter))
+        .pipe(eslint.failOnError());
+});
+
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('linter', ['lint']);
+
+gulp.task('serve', ['webserver']);
+
+gulp.task('build', ['webserver', 'lint', 'cleanDist', 'coreJs', 'featureJs', 'vendors', 'replace', 'watch']);
