e8590dd0417683f739febe602f0e5b3978d5b413
Alberto Gil <alberto.gil.contractor@bbva.com>
2015-08-15 13:59:46 +0200

    [mod] added new routes and controllers

--- www/app/core/app.route.js
+++ www/app/core/app.route.js
@@ -2,16 +2,31 @@
     'use strict';
 
     angular
-        .module('seed', ['ui.router'])
-        .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        .module('app', ['ui.router', 'app.series'])
+        .config(router);
 
-            $urlRouterProvider.otherwise('/actors');
+        router.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+        function router($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/series');
 
             $stateProvider
-                .state('/', {
-                    url:'/actors',
-                    templateUrl: 'app/actors/actors.html'
+                .state('series', {
+                    url:'/series',
+                    templateUrl: 'app/series/series.html',
+                    controller: 'SeriesCrtl',
+                    controllerAs: 'vm'
+                })
+                .state('detail', {
+                    url:'/detail',
+                    templateUrl: 'app/series/detail.html',
+                    controller: 'DetailCrtl',
+                    // controllerAs: 'vm'
+                })
+                .state('chapter', {
+                    url:'/chapter',
+                    templateUrl: 'app/series/chapter.html'
                     // controller: 'HomeController'
                 });
-        }]);
+        }
 })();
